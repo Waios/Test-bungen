0e40eefc63048411202c625291756a4c583acf4b
Michael Ruppert <michael.mi.ruppert@deutschebahn.com>
2018-04-23 20:04:38 +0200

    Beispiel Kommentare

--- src/main/java/de/merit/eval/azubi/ligaAnalyzer/csvInput/CSVReader.java
+++ src/main/java/de/merit/eval/azubi/ligaAnalyzer/csvInput/CSVReader.java
@@ -1,7 +1,5 @@
 package de.merit.eval.azubi.ligaAnalyzer.csvInput;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,27 +7,34 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CSVReader {
+public class CSVReader {  //Klasse; sie dient zum einlesen von CSV Dateiene die sich im src/main/java/resources Ordner befinden
 
 
-    public static List<String[]> read(String csvFile) {
+    public static List<String[]> read(String csvFile) {  // oeffentliche Klassenmethode (static) Rückgabe eine Liste mit String Array
+                                                         // im Array befinden sich die einzelnen Strings einer Zeile
+                                                         // Parameter: csvFile -> Dateiname mit Pfad der Datei die gelesen werden soll
 
-        BufferedReader br = null;
-        List<String[]> resultList = new ArrayList();
-        String line = "";
-        String cvsSplitBy = ";";
+        // Definitionen
+        BufferedReader br = null;                       // Lesepuffer fuer die Eingabezeilen
+        List<String[]> resultList = new ArrayList();    // Ergebnis Liste mit String Array[]
+        String line = "";                               // def Varible fuer eine Zeile
+        String cvsSplitBy = ";";                        // def des Trennzeichens
 
-        try {
+        try {                                           // try catch Block falls bei den Datei Operationen ein Fehler auftritt
 
-            br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
+            br = new BufferedReader(new FileReader(csvFile));     // in br wird ein neuer BufferedReader gespeicher , welcher mit
+                                                                  // einem neuen FileReader initialisiert wird. Der FileReader bekommt
+                                                                  // die Variable csvFile in der der Dateiname steht übergeben.
+
+            while ((line = br.readLine()) != null) {    // Schleife: solange der br eine Zeile liefert wird diese in der Variable line gespeichert
 
                 // use semicolon as separator
-                String[] words = line.split(cvsSplitBy);
-                resultList.add(words);
+                String[] words = line.split(cvsSplitBy);    // der Inhalt der Variablen line wird an den Trennzeichen (am Semikolon)
+                                                            // aufgesplittet und in der Variablen words als Array gespeichert
+                resultList.add(words);                      // das Array mit den Woertern wird der Ergebnis Liste hinzugefügt
 
             }
-
+        // Fehlerbehandlung
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -43,7 +48,8 @@ public class CSVReader {
                 }
             }
         }
-        return resultList;
+
+        return resultList; //Rückgabe der Liste mit den Wort Arrays
 
     }
 }
